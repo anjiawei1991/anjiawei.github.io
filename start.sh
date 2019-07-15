e44092b2fc2bec07a1bf01a460d994c80e3c17b4
anjiawei <sword2ya@qq.com>
2019-07-15 19:55:11 +0800

    开始使用k8s

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker stop beautiful-jekyll
-sudo docker run --rm -d -p 4000:4000 --name beautiful-jekyll -v "$PWD":/srv/jekyll anjiawei/beautiful-jekyll
+docker stop beautiful-jekyll
+docker run --rm -d -p 0.0.0.0:4000:4000 --name beautiful-jekyll -v "$PWD":/srv/jekyll anjiawei/beautiful-jekyll
